48d1255f8516e8423579852ef8c40d32762ebbdf
Ofer Schonberger <ofer.shin@gmail.com>
2020-03-10 17:39:44 +0200

    connection to db seems to work

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,16 @@
 var express = require('express');
 var router = express.Router();
-
+var query = require('../db/connector');
 var users = {1 : 'user1', 2: 'user2', 3: 'user3'};
 
 /* GET users listing. */
 router.get('/users', function (req, res, next) {
-  res.send('respond with a resource 1111asdf' + users[1]);
+  var result;
+  query.query('SELECT * from playground').then((data)=> {
+    console.log('data ' + JSON.stringify(data.rows));
+    res.send('respond with a resource 1111asdf' + users[1] + ' result : ' + data.rows);
+    result = data;
+  });
 });
 
 module.exports = router;
