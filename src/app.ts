55f06f9751ad18c9b13eef5094ae9015aa5665f7
Shani Schwartz <shanis@Shanis-MacBook-Pro.local>
2020-03-30 13:57:38 +0300

    cleanups from previous project

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,3 @@
-import './loadEnv';
 var createError = require('http-errors');
 import express = require('express');
 var path = require('path');
@@ -11,14 +10,8 @@ const app: express.Application = express();
 app.use(express.static('public'));
 app.use(router);
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(logger('dev'));
-app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // catch 404 and forward to error handler
@@ -33,15 +26,5 @@ app.listen(port, () => {
     console.log('Express server started on port: ' + port);
 });
 
-// error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
 
 module.exports = app;
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-router.use(require('./users'));
 router.use(require('./index'));
 
 module.exports = router;
